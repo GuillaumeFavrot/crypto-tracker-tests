b52c75bb96d88f7c3415b6b5a6eacb30f24e3dd1
Guillaume Favrot <gui.favrot@gmail.com>
2023-01-05 07:53:43 +0100

    comments to docstring conversion

--- test_views.py
+++ test_views.py
@@ -7,36 +7,41 @@ url = 'http://172.104.146.107'
 
 class TestViews(unittest.TestCase):
 
-    # First test, I try to access the app on the root address
     def test_getMain(self):
+        """First test, I try to access the app on the root address"""
         self.assertEqual(requests.get(url).status_code, 200)
     
-    # Second test, I try to retrieve my wallet data 
     def test_getWallet(self):
+        """Second test, I try to retrieve my wallet data"""
         self.assertEqual(requests.get(f'{url}/api/wallet').status_code, 200)
 
-    # Third test, I try to update my wallet by adding new tokens using a valid request
     def test_updateWallet_valid_buy(self):
+        """Third test, I try to update my wallet by adding new tokens using a valid request"""
         data = {"token" : "BTC", "quantity" : 1, "buying_value" : 15000}
         self.assertEqual(requests.put(f'{url}/api/wallet',json=data).status_code, 200)
 
-    # Fourth test, I try to update my wallet by adding new tokens using a invalid request 
-    # (note that all form validation are done on the frontend of the app hence the 500 status code)
     def test_updateWallet_invalid(self):
+        """Fourth test, I try to update my wallet by adding new tokens using a invalid request"""
+        """(note that all form validation are done on the frontend of the app hence the 500 status code)"""
         data = {"token" : 1, "quantity" : 'aaaa', "buying_value" : True}
         self.assertEqual(requests.put(f'{url}/api/wallet',json=data).status_code, 500)
 
-    # Fifth test, I try to backup the data of my new wallet (1 BTC)
     def test_history_backup(self):
+        """Fifth test, I try to backup the data of my new wallet (1 BTC)"""
         self.assertEqual(requests.get(f'{url}/api/history/backup').status_code, 200)
 
-    # Sixth test, I try to request a report then get a plot id and finally retrive a plot with the id 
     def test_request_a_report(self):
+        """Sixth test, I try to request a report then get a plot id using a valid request"""
         data = {"token" : "total", "report" : "profit", "period" : "all"}
         self.assertEqual(requests.post(f'{url}/api/history/get',json=data).status_code, 200)
-    
-    # seventh test, I try to get access to my history data    
+
+    def test_request_a_report(self):
+        """Seventh test, I try to request a report then get a plot id unsing an invalid request"""
+        data = {"token" : "Jesuscoin", "report" : "profit", "period" : "all"}
+        self.assertEqual(requests.post(f'{url}/api/history/get',json=data).status_code, 500)
+
     def test_get_graph(self):
+        """Eighth test, I try to ask for a report, get a plot ID and access the plot"""
         data = {"token" : "total", "report" : "profit", "period" : "all"}
         response = requests.post(f'{url}/api/history/get',json=data)
         graph_id = response.text
